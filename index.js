19812aa2ec20834b4722d1a39d3b922cf0443b67
emannueljackson <130757039+emannueljackson@users.noreply.github.com>
2023-11-30 00:23:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function somatorioDivisiveis35(numero) {
+    // Inicializando a variável de somatório
+    let somatorio = 0;
+
+    // Iterando de 0 até o número (exclusive)
+    for (let i = 1; i < numero; i++) {
+        // Verificando se o número é divisível por 3 ou 5
+        if (i % 3 === 0 || i % 5 === 0) {
+            // Adicionando ao somatório
+            somatorio += i;
+        }
+    }
+
+    // Retornando o resultado
+    return somatorio;
+}
+
+const numeroLimite = parseInt(prompt("Digite um número inteiro:"));
+
+// Chama a função e exibe o resultado
+const resultadoSoma = somatorioDivisiveis35(numeroLimite);
+console.log('A soma dos números divisíveis por 3 e 5 até ${numeroLimite} é: ${resultadoSoma}');
